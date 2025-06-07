738fa5153b5718f9c388e6da3b0c0567b0fbb7d7
Thor <thor_parsons@hotmail.com>
2025-06-07 12:00:16 -0700

    permissions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,8 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet, basename='profile')
+
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
